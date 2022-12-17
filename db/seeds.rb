91b790f1d0bad6ca50ff06c33cfa252791dc3df6
vojtech matous <vojtech.matous@juicymo.cz>
2022-12-17 12:59:52 +0100

    Add basic seeds and book icon

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+User.create!(email: "fidan@movies.com", password: "12345678")
+User.create!(email: "master.commander@movies.com", password: "12345678")
+User.create!(email: "vojtech@movies.com", password: "12345678")
