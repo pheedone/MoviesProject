ce3e8ae1d53ae673b63a1ce7d887d89ee3e9c27d
vojtech matous <vojtech.matous@juicymo.cz>
2022-12-20 00:07:26 +0100

    Improve show

--- app/controllers/my_movies_controller.rb
+++ app/controllers/my_movies_controller.rb
@@ -6,4 +6,18 @@ class MyMoviesController < ApplicationController
   def show
     @movie = MyMovie.find(params[:id])
   end
+
+  # TODO review and add a button for it in show and in index on the right
+  # def change_description
+  #   @movie = MyMovie.find(params[:id])
+  #   @movie.update(my_movie_params)
+  #   redirect_to my_movie_path(@movie)
+  # end
+
+  private
+
+  # allow only centain parameters to be passed to the controller
+  # def my_movie_params
+  #   params.require(:my_movie).permit(:description)
+  # end
 end
